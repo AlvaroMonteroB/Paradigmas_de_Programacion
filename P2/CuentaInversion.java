08b5a860e749a26a18296a2470913384c137bd71
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2023-05-16 20:01:49 -0600

    cuenta fin

--- P2/CuentaInversion.java
+++ P2/CuentaInversion.java
@@ -40,26 +40,24 @@ public class CuentaInversion{
     }
 
     public static void main(String[] args){
-        CuentaInversion cuenta= new CuentaInversion("dll", 0.0, 20.0, "Alvaro");
+        CuentaInversion cuenta= new CuentaInversion("2022630403", 0.0, .08, "Alvaro");
         //Double aux=0.0;
         System.out.print("Digite la cantidad a depositar\n");
         Scanner scan=new Scanner(System.in);
         cuenta.deposito(scan.nextDouble());
-        scan.close();
         System.out.print("Digite la cantidad a retirar\n");
-        Scanner scan2=new Scanner(System.in);
-        boolean a=cuenta.retiro(scan2.nextDouble());
+        boolean a=cuenta.retiro(scan.nextDouble());
         if(a){
             System.out.print("Retiro efectuado con exito\n");
         }else{
             System.out.print("Retiro no efectuado\n");
-        }
-        scan2.close();    
+        }   
         double interes;
         interes=cuenta.Calc_inter();
+        scan.close();
             System.out.print("Tu interés es de ");
             System.out.print(interes);
-        System.out.print("\nSeñor "+cuenta.nombre+"El balance de tu cuenta es "+cuenta.obt_balance()+'\n'+"id: "+cuenta.Id);
+        System.out.print("\nSeñor "+cuenta.nombre+", El balance de tu cuenta es "+cuenta.obt_balance()+'\n'+"id: "+cuenta.Id);
         
 
     }
